ff962f266631e88bb4de9dde914b5778d1233f51
qianqiuyueying <3044481323@qq.com>
2025-01-22 18:07:09 +0800

    modified:   dockerenvs/mysql/conf/my.cnf 	deleted:    dockerfiles/Dockerfile 	deleted:    init.py 	new file:   init.sh 	renamed:    main.py -> manage.py

---
+++ init.sh
@@ -0,0 +1,13 @@
+# mysql 创建随机密码
+# 创建随机密码
+export MYSQL_ROOT_PASSWORD=$(openssl rand -base64 12)
+# 随机密码存入.env文件
+echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" > .env
+
+
+
+
+
+
+# 启动容器
+docker-compose up -d
