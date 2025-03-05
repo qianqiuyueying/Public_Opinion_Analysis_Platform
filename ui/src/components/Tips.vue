0729a7e3ffcfd6d9f7510d798948d31b5207182a
qianqiuyueying <3044481323@qq.com>
2025-03-05 13:00:37 +0800

    Changes to be committed: 	new file:   ui/src/components/Tips.vue
    Changes not staged for commit:
    	modified:   __pycache__/manage.cpython-312.pyc
    	modified:   app/__init__.py
    	modified:   app/__pycache__/__init__.cpython-312.pyc
    	modified:   app/scrapy/__pycache__/controller.cpython-312.pyc
    	modified:   app/scrapy/__pycache__/service.cpython-312.pyc
    	modified:   app/scrapy/controller.py
    	modified:   app/scrapy/model/__pycache__/data_model.cpython-312.pyc
    	modified:   app/scrapy/model/__pycache__/dto.cpython-312.pyc
    	modified:   app/scrapy/model/dto.py
    	modified:   app/scrapy/service.py
    	modified:   app/user/controller/__pycache__/userController.cpython-312.pyc
    	modified:   app/user/controller/userController.py
    	modified:   app/user/model/__pycache__/data_model.cpython-312.pyc
    	modified:   app/user/model/data_model.py
    	modified:   app/user/repository/__pycache__/userRespository.cpython-312.pyc
    	modified:   app/user/repository/userRespository.py
    	modified:   app/user/service/__pycache__/userService.cpython-312.pyc
    	modified:   app/user/service/userService.py
    	modified:   manage.py
    	modified:   test.py
    	modified:   ui/src/api/scrapyService.js
    	modified:   ui/src/api/userService.js
    	modified:   ui/src/pages/scrapy/MakeSpider.vue
    	modified:   ui/src/pages/scrapy/Spider.vue
    	modified:   ui/src/pages/scrapy/components/MakeRules.vue
    	modified:   ui/src/pages/scrapy/components/ProductSpider.vue
    	modified:   ui/src/pages/scrapy/components/RequestConfig.vue
    	modified:   ui/src/pages/scrapy/components/ResultView.vue
    	modified:   ui/src/pages/scrapy/components/SpiderClass.vue
    	modified:   ui/src/pages/scrapy/components/TestLink.vue
    	modified:   ui/src/pages/user/Login.vue
    	modified:   ui/src/pages/user/Register.vue
    	modified:   ui/src/utils/request.js
    	modified:   util/__pycache__/tool.cpython-312.pyc
    	modified:   util/tool.py
    
    Untracked files:
    	util/spider/

---
+++ ui/src/components/Tips.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-tooltip :content="content" :placement="placement">
+    <el-icon size="16"><QuestionFilled/></el-icon>
+  </el-tooltip>
+</template>
+
+<script setup>
+import {QuestionFilled} from "@element-plus/icons-vue";
+
+const content = defineModel("content", {
+  type: String,
+})
+const placement = defineModel("placement", {
+  type: String,
+  default: 'top'
+})
+
+</script>
+
+
+<style scoped>
+
+</style>
